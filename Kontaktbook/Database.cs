a8c722b66f7cea284c2656eed450f037e75571a5
Ömer <zedxurl@gmail.com>
2022-04-12 22:19:35 +0200

    summarowanie
    commitowanie i pushowanie

---
+++ Kontaktbook/Database.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Windows;
+
+namespace Kontaktbook
+{
+    public class Database
+    {
+        protected static string connstr = "Data Source=library.db;Version=3;";
+
+
+        public static DataView GetUser()
+        {
+            using (SQLiteConnection con = new SQLiteConnection(connstr))
+            {
+                con.Open();
+                SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter("SELECT * FROM kontaks", con);
+                con.Close();
+                DataSet dataSet = new DataSet();
+                dataAdapter.Fill(dataSet);
+
+                return dataSet.Tables[0].DefaultView;
+
+            }
+
+        }
+
+        public static int PutUser(Kontakt kontakt)
+        {
+
+            using (SQLiteConnection con = new SQLiteConnection(connstr))
+            {
+                int result = -1;
+
+                con.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand(con))
+                {
+                    cmd.CommandText = "INSERT INTO kontaks(name, surname, phonenumber, email, birthdate) VALUES (@name, @surname, @phonenumber, @email, @birthdate)";
+                    cmd.Prepare();
+                    cmd.Parameters.AddWithValue("@name", kontakt.Name);
+                    cmd.Parameters.AddWithValue("@surname", kontakt.Surname);
+                    cmd.Parameters.AddWithValue("@phonenumber", kontakt.PhoneNumber);
+                    cmd.Parameters.AddWithValue("@email", kontakt.Email);
+                    cmd.Parameters.AddWithValue("@birthdate", kontakt.Birthdate);
+                    try
+                    {
+                        result = cmd.ExecuteNonQuery();
+
+                    }
+                    catch (SQLiteException e)
+                    {
+                        MessageBox.Show(e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return -1;
+                    }
+                    con.Close();
+                    return result;
+                }
+
+            }
+        }
+
+        public static int UpdateUser(Kontakt kontakt)
+        {
+
+            using (SQLiteConnection con = new SQLiteConnection(connstr))
+            {
+                int result = -1;
+                con.Open();
+                using (SQLiteCommand command = new SQLiteCommand(con))
+                {
+                    command.CommandText = "UPDATE kontaks SET name = @name, surname = @surname, phonenumber = @phonenumber, email = @email, birthdate = @birthdate WHERE id = @id";
+                    command.Prepare();
+                    command.Parameters.AddWithValue("@name", kontakt.Name);
+                    command.Parameters.AddWithValue("@surname", kontakt.Surname);
+                    command.Parameters.AddWithValue("@phonenumber", kontakt.PhoneNumber);
+                    command.Parameters.AddWithValue("@email", kontakt.Email);
+                    command.Parameters.AddWithValue("@birthdate", kontakt.Birthdate);
+                    command.Parameters.AddWithValue("@id", kontakt.userID);
+                    try
+                    {
+                        result = command.ExecuteNonQuery();
+
+                    }
+                    catch (SQLiteException e)
+                    {
+                        MessageBox.Show(e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return -1;
+                    }
+
+
+                }
+                con.Close();
+                return result;
+            }
+
+        }
+
+        public static int DeleteUser(int id)
+        {
+
+            using (SQLiteConnection con = new SQLiteConnection(connstr))
+            {
+
+                int result = -1;
+                con.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand(con))
+                {
+
+                    cmd.CommandText = "DELETE FROM kontaks WHERE id = @id";
+                    cmd.Prepare();
+                    cmd.Parameters.AddWithValue("@id", id);
+                    try
+                    {
+                        result = cmd.ExecuteNonQuery();
+                    }
+                    catch (SQLiteException e)
+                    {
+                        MessageBox.Show(e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return -1;
+                    }
+                }
+                con.Close();
+                return result;
+            }
+        }
+
+        public static Kontakt FindUser(int id)
+        {
+
+            using (SQLiteConnection con = new SQLiteConnection(connstr))
+            {
+                con.Open();
+                using (SQLiteCommand command = new SQLiteCommand("SELECT * FROM kontaks WHERE id = @id LIMIT 1", con))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    SQLiteDataReader reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        Kontakt kontakt = new Kontakt
+                        {
+                            Name = reader[1].ToString(),
+                            Surname = reader[2].ToString(),
+                            PhoneNumber = reader[3].ToString(),
+                            Email = reader[4].ToString(),
+                            Birthdate = reader[5].ToString(),
+                            userID = Int32.Parse(reader[0].ToString())
+                        };
+
+                        return kontakt;
+                    }
+
+
+                }
+
+                con.Close();
+                return new Kontakt();
+            }
+
+        }
+    }
+}
---
+++ Kontaktbook/MainWindow.xaml.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Data;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Kontaktbook
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            DataRowView row = (DataRowView)dataGrid.SelectedItem;
+            int id = Int32.Parse(row.Row.ItemArray[0].ToString());
+            Kontakt kontakt = Database.FindUser(id);
+            ShowUser(kontakt);
+
+
+        }
+
+        private void btnsubmit_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (ItsnotEmpty())
+            {
+                AddNewUser();
+            }
+
+
+        }
+
+        private void btnclear_Click(object sender, RoutedEventArgs e)
+        {
+            Clearfields();
+
+        }
+
+        private void btnrefresh_Click(object sender, RoutedEventArgs e)
+        {
+
+            Refresh();
+
+        }
+
+        private void btndelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(labelid.Content.ToString()))
+            {
+                MessageBox.Show("Please select user from the list", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            if (MessageBox.Show("Do you really want to delete selected user?", "Delete user", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                int id = Int32.Parse(labelid.Content.ToString());
+                Database.DeleteUser(id);
+                Clearfields();
+                Refresh();
+            }
+
+        }
+
+        private void btnupdate_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtName.Text))
+            {
+                MessageBox.Show("Please select user from the list", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            if (ItsnotEmpty())
+            {
+                Kontakt kontakt = new Kontakt
+                {
+                    userID = Int32.Parse(labelid.Content.ToString()),
+                    Name = txtName.Text,
+                    Surname = txtSurname.Text,
+                    PhoneNumber = txtPhone.Text,
+                    Email = txtemail.Text,
+                    Birthdate = btndatepicker.Text
+
+                };
+
+
+
+                if (Database.UpdateUser(kontakt) != -1)
+                {
+
+                    MessageBox.Show("User updated successfuly", "Update user", MessageBoxButton.OK, MessageBoxImage.Information);
+                    Clearfields();
+                    Refresh();
+                    return;
+                }
+
+            }
+
+            else
+            {
+                MessageBox.Show("An error occured", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+
+        }
+
+
+        private void Refresh()
+        {
+            dataGrid.ItemsSource = Database.GetUser();
+        }        
+
+        private void Clearfields()
+        {
+            txtName.Text = String.Empty;
+            txtSurname.Text = String.Empty;
+            txtPhone.Text = String.Empty;
+            txtemail.Text = String.Empty;
+            btndatepicker.Text = String.Empty;
+            labelid.Content = String.Empty;
+
+        }        
+
+        private bool ItsnotEmpty()
+        {
+
+            if (String.IsNullOrEmpty(txtemail.Text) || String.IsNullOrEmpty(txtName.Text) || String.IsNullOrEmpty(txtSurname.Text)
+                || String.IsNullOrEmpty(txtPhone.Text) || String.IsNullOrEmpty(btndatepicker.Text))
+            {
+                MessageBox.Show("All fields are required");
+                return false;
+            }
+            return true;
+        }
+
+        private void AddNewUser()
+        {
+
+            Kontakt kontakt = new Kontakt
+            {
+                Name = txtName.Text,
+                Surname = txtSurname.Text,
+                PhoneNumber = txtPhone.Text,
+                Email = txtemail.Text,
+                Birthdate = btndatepicker.Text
+            };
+            if (Database.PutUser(kontakt) != -1)
+            {
+
+                MessageBox.Show("User Added Successfuly", "New user", MessageBoxButton.OK, MessageBoxImage.Information);
+                Clearfields();
+                Refresh();
+            }
+            else
+            {
+                MessageBox.Show("An error occured", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+        }
+
+        private void ShowUser(Kontakt kontakt)
+        {
+            labelid.Content = kontakt.userID;
+            txtName.Text = kontakt.Name;
+            txtSurname.Text = kontakt.Surname;
+            txtPhone.Text = kontakt.PhoneNumber;
+            txtemail.Text = kontakt.Email;
+            btndatepicker.Text = kontakt.Birthdate;
+        }
+
+    }
+}
